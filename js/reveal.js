cc1072980537a74d079f653a6cb53667a0c9c265
SucInnoPeneur <elizabeth-win@hotmail.com>
2016-02-17 21:33:38 -0800

    Update reveal.js

--- js/reveal.js
+++ js/reveal.js
@@ -35,8 +35,8 @@
 
 			// The "normal" size of the presentation, aspect ratio will be preserved
 			// when the presentation is scaled to fit different resolutions
-			width: auto,
-			height: auto,
+			width: 100%,
+			height: 100%,
 
 			// Factor of the display size that should remain empty around the content
 			margin: 0.1,
